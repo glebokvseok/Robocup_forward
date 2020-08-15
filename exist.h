573bc366fb491b3bb0948a4c4c7b51a3c5796a07
gleb321 <gleb@evlakhov.com>
2020-08-15 13:47:54 +0300

    new line calculation

--- exist.h
+++ exist.h
@@ -16,44 +16,44 @@ struct camera {
 };
 
 class Math {
-	private:
-		const double central_x = 164;
-		const double central_y = 115;
-		const double front_x = 175; 
-		const double front_y = 116; 
-	public:
-		int sign(double x);
-		double radian(double angle); 
-		double distance(int x, int y);
-		double countDistance(double d);
-		double countAngle(double ball_x, double ball_y);
+  	private:
+    		const double central_x = 164;
+    		const double central_y = 115;
+    		const double front_x = 175; 
+    		const double front_y = 116; 
+  	public:
+    		int sign(double x);
+    		double radian(double angle); 
+    		double distance(int x, int y);
+    		double countDistance(double d);
+    		double countAngle(double ball_x, double ball_y);
 };
 
 class Robot {
-	private:
-		const byte left_button_port = 23;
-		const byte right_button_port = 22;
-		const byte led_digital_port[3] = {33, 35, 37};
-		const byte motors_in1[4] = {38, 42, 28, 3};
-		const byte motors_in2[4] = {36, 40, 30, 5};
-		const byte motors_pwm[4] = {6, 44, 10, 12};
-    int calibration_value[24] = {581, 605, 582, 605, 586, 547, 622, 687, 629, 632, 542, 644, 630, 596, 551, 591, 657, 639, 544, 643, 535, 539, 520, 571};
-		double led_angle[24];
-		const double angle_coef = 0.785398163397448;
-
-	public:
-		int degree;
-    Pixy2 pixy;
-	public:
-		void init();
-		bool buttonPressed(byte n);
-		int readChannel(int n, int m);
-		double updateLed();
-		void runMotor(byte port, short speed);
-		void moveAngle(double angle, short speed, int u);
-		void updateGyro();
-    int updateCamera(int signature[], int n);
-    bool setTimer(long long timer, int dt);
+  	private:
+    		const byte left_button_port = 23;
+    		const byte right_button_port = 22;
+    		const byte motors_in1[4] = {38, 42, 28, 3};
+    		const byte motors_in2[4] = {36, 40, 30, 5};
+    		const byte motors_pwm[4] = {6, 44, 10, 12};
+        const byte led_digital_port[3] = {33, 35, 37};
+        int calibration_value[24] = {569, 592, 599, 585, 568, 540, 605, 676, 604, 616, 516, 620, 628, 594, 550, 582, 647, 627, 534, 633, 534, 528, 509, 557};
+    		const double angle_coef = 0.785398163397448;
+  	public:
+    		int degree;
+        Pixy2 pixy;
+        double led_angle[24];
+        bool led_value[24];
+  	public:
+    		void init();
+        void updateGyro();
+    		bool buttonPressed(byte n);
+    		int readChannel(int n, int m);
+    		bool updateLed(bool led_value[]);
+    		void runMotor(byte port, short speed);
+        bool setTimer(long long timer, int dt);
+        int updateCamera(int signature[], int n);
+    		void moveAngle(double angle, short speed, int u);
 };
 
 #endif
